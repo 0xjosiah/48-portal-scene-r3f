dea12021a7e6cb827361e6e4089a92600ed2fb6a
Josiah Webb <jj.webb520@gmail.com>
2023-01-25 20:18:54 -0700

    refactors portal scene into its own component

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,33 +1,34 @@
 import { Center, OrbitControls, shaderMaterial, Sparkles, useGLTF, useTexture } from '@react-three/drei'
-import { extend, useFrame } from '@react-three/fiber'
-import * as THREE from 'three'
-import portalVertexShader from './shaders/portal/vertex.glsl'
-import portalFragmentShader from './shaders/portal/fragment.glsl'
-import { useRef } from 'react'
+// import { extend, useFrame } from '@react-three/fiber'
+// import * as THREE from 'three'
+// import portalVertexShader from './shaders/portal/vertex.glsl'
+// import portalFragmentShader from './shaders/portal/fragment.glsl'
+// import { useRef } from 'react'
+import { Portal } from './Portal'
 
 // advantage with this: can use in multiple places, easier to adjust
-const PortalMaterial = shaderMaterial(
-    {
-        uTime: 0,
-        uColorStart: new THREE.Color(0xffffff),
-        uColorEnd: new THREE.Color(0x000000)
-    },
-    portalVertexShader,
-    portalFragmentShader
-)
+// const PortalMaterial = shaderMaterial(
+//     {
+//         uTime: 0,
+//         uColorStart: new THREE.Color(0xffffff),
+//         uColorEnd: new THREE.Color(0x000000)
+//     },
+//     portalVertexShader,
+//     portalFragmentShader
+// )
 
-extend({ PortalMaterial }) // this extend helper makes a class available as jsx
+// extend({ PortalMaterial }) // this extend helper makes a class available as jsx
 
 export default function Experience()
 {
-    const { nodes } = useGLTF('./model/portal.glb')
-    const bakedTexture = useTexture('./model/baked.jpg')
-    bakedTexture.flipY = false // this is true by default
+    // const { nodes } = useGLTF('./model/portal.glb')
+    // const bakedTexture = useTexture('./model/baked.jpg')
+    // bakedTexture.flipY = false // this is true by default
 
-    const portalMaterialRef = useRef(null)
-    useFrame((state, delta) => {
-        portalMaterialRef.current.uTime += delta
-    })
+    // const portalMaterialRef = useRef(null)
+    // useFrame((state, delta) => {
+        // portalMaterialRef.current.uTime += delta
+    // })
 
     return <>
         <color args={ [ '#030202' ] } attach="background" />
@@ -35,34 +36,38 @@ export default function Experience()
         <OrbitControls makeDefault />
 
         <Center>
+            <Portal />
+        </Center>
+
+        {/* <Center> */}
 
             {/* the base scene */}
-            <mesh geometry={nodes.baked.geometry}>
-                <meshBasicMaterial map={ bakedTexture }
+            {/* <mesh geometry={nodes.baked.geometry}> */}
+                {/* <meshBasicMaterial map={ bakedTexture }
                     // map-flipY={ false } // this another way to flip the texture
-                />
-            </mesh>
+                /> */}
+            {/* </mesh> */}
 
             {/* the lights */}
-            <mesh
+            {/* <mesh
                 geometry={ nodes.poleLightA.geometry }
                 position={ nodes.poleLightA.position }
-            >
-                <meshBasicMaterial color={0xffffe5} />
-            </mesh>
-            <mesh
+            > */}
+                {/* <meshBasicMaterial color={0xffffe5} /> */}
+            {/* </mesh> */}
+            {/* <mesh
                 geometry={ nodes.poleLightB.geometry }
                 position={ nodes.poleLightB.position }
-            >
-                <meshBasicMaterial color={0xffffe5} />
-            </mesh>
+            > */}
+                {/* <meshBasicMaterial color={0xffffe5} /> */}
+            {/* </mesh> */}
 
             {/* the portal */}
-            <mesh
+            {/* <mesh
                 geometry={ nodes.portalLight.geometry }
                 position={ nodes.portalLight.position }
                 rotation={ nodes.portalLight.rotation }
-            >
+            > */}
                 {/* traditional shaderMaterial from three.js */}
                 {/* <shaderMaterial
                     vertexShader={ portalVertexShader }
@@ -75,19 +80,19 @@ export default function Experience()
                 /> */}
 
                 {/* shaderMaterial from drei */}
-                <portalMaterial ref={ portalMaterialRef } />
-            </mesh>
+                {/* <portalMaterial ref={ portalMaterialRef } /> */}
+            {/* </mesh> */}
 
             {/* fireflies */}
-            <Sparkles 
+            {/* <Sparkles 
                 count={ 50 }
                 size={ 6 }
                 scale={ [ 4, 2, 4 ] }
                 position-y={ 1 }
                 speed={ 0.35 }
-            />
+            /> */}
 
-        </Center>
+        {/* </Center> */}
 
     </>
 }
